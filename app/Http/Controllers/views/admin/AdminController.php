208e40d0f7632f168d75d5ee24a6c15155cdbabd
Hyacinthe ABANDA <hyacinthabanda@gmail.com>
2019-11-19 07:28:29 +0100

    dashbord search

--- app/Http/Controllers/views/admin/AdminController.php
+++ app/Http/Controllers/views/admin/AdminController.php
@@ -5,22 +5,55 @@ namespace App\Http\Controllers\views\admin;
 use Auth;
 use App\Models\User;
 use App\Models\Etudiant;
+use App\Models\Formation;
 use App\Models\CommuneFormation;
 use App\Models\Formateur;
+use App\Models\Region;
+use App\Models\Departement;
+use App\Models\Commune;
+use App\Models\Thematique;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
-    public function dashboard ()
+    public function dashboard (Request $request)
     {
-        $user  = User::whereIsActive(true)->whereId(Auth::id())->first();
-        $users  = User::whereIsActive(true)->count();
-        $etudiants  = Etudiant::count();
-        $formateurs  = Formateur::count();
-        $formations  = CommuneFormation::count();
+        $user          = User::whereIsActive(true)->whereId(Auth::id())->first();
+        $users         = User::whereIsActive(true)->get();
+        $etudiants     = Etudiant::get();
+        $formateurs    = Formateur::get();
+        $formations    = CommuneFormation::with('formation', 'commune')->get();
+        $communes      = Commune::get();
+        $departements  = Departement::get();
+        $regions       = Region::get();
+        $thematiques   = Thematique::get();
 
-        return view('admin.all.dashboard', compact('users', 'etudiants', 'user', 'formateurs', 'formations'));
+        $search = Formation::with('sites', 'sites.formation', 'sites.commune', 'sites.commune.departement', 'sites.commune.departement', 'phases', 'phases.thematiques')
+                      ->when($request->commune_id, function ($q) use ($request) {
+                          return $q->whereHas('sites', function($sql) use ($request) {
+                              return $sql->where('commune_id', $request->commune_id);
+                          });
+                      })
+                      ->when($request->departement_id, function ($q) use ($request) {
+                          return $q->whereHas('sites.commune', function($sql) use ($request) {
+                              return $sql->where('departement_id', $request->departement_id);
+                          });
+                      })
+                      ->when($request->region_id, function ($q) use ($request) {
+                          return $q->whereHas('sites.commune.departement', function($sql) use ($request) {
+                              return $sql->where('region_id', $request->region_id);
+                          });
+                      })
+                      ->when($request->thematique_id, function ($q) use ($request) {
+                          return $q->whereHas('phases.thematiques', function($sql) use ($request) {
+                              return $sql->where('id', $request->thematique_id);
+                          });
+                      })
+                      ->get();
+
+        return view('admin.all.dashboard', compact('users', 'etudiants', 'search',  'user', 'formateurs', 'formations', 'regions', 'departements', 'communes', 'thematiques'));
     }
+
 
 }
--- resources/views/admin/all/dashboard.blade.php
+++ resources/views/admin/all/dashboard.blade.php
@@ -12,31 +12,123 @@
         <div class="cards row">
             <div class="col-sm-3">
                 <div class="card blue">
-                    <h3>{{ $users }}</h3>
+                    <h3>{{ count($users) }}</h3>
                     <h5>Utilisateurs</h5>
                 </div>
             </div>
 
             <div class="col-sm-3">
                 <div class="card red">
-                    <h3>{{ $etudiants }}</h3>
+                    <h3>{{ count($etudiants) }}</h3>
                     <h5>Stagiaires</h5>
                 </div>
             </div>
 
             <div class="col-sm-3">
                 <div class="card green">
-                    <h3>{{ $formateurs }}</h3>
+                    <h3>{{ count($formateurs) }}</h3>
                     <h5>Formateurs</h5>
                 </div>
             </div>
 
             <div class="col-sm-3">
                 <div class="card dark">
-                    <h3>{{ $formations }}</h3>
+                    <h3>{{ count($formations) }}</h3>
                     <h5>Formations</h5>
                 </div>
             </div>
+        </div>
+
+        <div class="row mt-20 bg-white">
+          <form class="_form pt-20" action="" method="get">
+            <div class="col-sm-3">
+                <div class="form-select grey">
+                    <select name="region_id" class="form-control input-lg">
+                        <option value="">Toutes les régions</option>
+                        @foreach ($regions as $item)
+                            <option value="{{ $item->id }}" {{ Request::get('region_id') == $item->id ? 'selected' : '' }}>{{ $item->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="form-select grey">
+                    <select name="departement_id" class="form-control input-lg">
+                        <option value="">Tous les départements</option>
+                        @foreach ($departements as $item)
+                            <option value="{{ $item->id }}" {{ Request::get('departement_id') == $item->id ? 'selected' : '' }}>{{ $item->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="form-select grey">
+                    <select name="commune_id" class="form-control input-lg">
+                        <option value="">Toutes les communes</option>
+                        @foreach ($communes as $item)
+                            <option value="{{ $item->id }}"{{ Request::get('commune_id') == $item->id ? 'selected' : '' }}>{{ $item->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <div class="col-sm-3">
+                <div class="form-select grey">
+                    <select name="thematique_id" class="form-control input-lg">
+                        <option value="">Toutes les thématiques</option>
+                        @foreach ($thematiques as $item)
+                            <option value="{{ $item->id }}"{{ Request::get('thematique_id') == $item->id ? 'selected' : '' }}>{{ $item->name }} : {{ $item->duree }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <div class="col-sm-3 mt-20">
+                <div class="form-group">
+                    <input type="datetime-local" name="start_date" class="form-control input-lg">
+                </div>
+            </div>
+
+            <div class="col-sm-3 mt-20">
+                <div class="form-group">
+                    <input type="datetime-local" name="end_date" class="form-control input-lg">
+                </div>
+            </div>
+
+            <div class="col-sm-3 mt-20">
+                <button type="submit" class="btn btn-lg btn-primary btn-block bold">
+                    Rechercher
+                </button>
+            </div>
+          </form>
+
+          @if ($search)
+            <div class="col-sm-12 mt-40">
+              <table class="table table-striped">
+                  <thead>
+                      <tr>
+                          <th>Titre</th>
+                          <th>Status</th>
+                          <th>Nbre places</th>
+                          <th>Cree le</th>
+                      </tr>
+                  </thead>
+
+                  <tbody>
+                      @foreach($search as $item)
+                          <tr data-href="">
+                              <td class="bold">{{ $item->title }}</td>
+                              <td class="text-center">{{ $item->is_active ? 'Active' : 'Non active' }}</td>
+                              <td class="text-center">{{ $item->qte_requis }}</td>
+                              <td class="text-center">{{ date('d/m/Y H:i', strtotime($item->created_at)) }}</td>
+                          </tr>
+                      @endforeach
+                  </tbody>
+              </table>
+            </div>
+          @endif
         </div>
     </div>
 </div>
